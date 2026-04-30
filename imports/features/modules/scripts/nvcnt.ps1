8963dbf3aef7e647da33407806461a44b3268dda
Ropp <58117887+ropp5pop@users.noreply.github.com>
2026-04-30 19:19:58 +0200

    support for missing service

--- imports/features/modules/scripts/nvcnt.ps1
+++ imports/features/modules/scripts/nvcnt.ps1
@@ -4,14 +4,24 @@
 param([string]$Action)
 $ServiceName = "NVDisplay.ContainerLocalSystem"
 $AppID = "NVIDIACorp.NVIDIAControlPanel_56jybvy8sckqj!NVIDIACorp.NVIDIAControlPanel"
+$Service = Get-Service $ServiceName -ErrorAction SilentlyContinue
+
+if (-not $Service) {
+    if ($Action -ne "Stop") {
+        explorer.exe "shell:AppsFolder\$AppID"
+    }
+    return
+}
 
 if ($Action -eq "Stop") {
-    Start-Process powershell.exe -ArgumentList "-Command Stop-Service $ServiceName -Force" -Verb RunAs -Wait
-}
-else {
-    $svc = Get-Service $ServiceName -ErrorAction SilentlyContinue
-    if ($svc -and $svc.Status -ne "Running") {
-        Start-Process powershell.exe -ArgumentList "-Command Start-Service $ServiceName" -Verb RunAs -Wait
+    if ($Service.Status -ne "Stopped") {
+        Start-Process powershell.exe -ArgumentList "-Command Stop-Service '$ServiceName' -Force" -Verb RunAs -Wait
     }
-    explorer.exe "shell:AppsFolder\$AppID"
+    return
 }
+
+if ($Service.Status -ne "Running") {
+    Start-Process powershell.exe -ArgumentList "-Command Start-Service '$ServiceName'" -Verb RunAs -Wait
+}
+
+explorer.exe "shell:AppsFolder\$AppID"
